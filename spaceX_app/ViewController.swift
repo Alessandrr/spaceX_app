4812bfc595c0ebd2fd258c4493d3eb71f24911b3
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2023-06-20 16:07:55 +0400

    Initial Commit

---
+++ spaceX_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spaceX_app
+//
+//  Created by Aleksandr Mamlygo on 20.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
